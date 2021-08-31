9c8c0b050fa1edf7f03a73543ef9fd0b5a6dcf17
kevin-m <kmclaughlinbtb@gmail.com>
2021-08-31 18:45:46 -0500

    updated settings

--- schedulr_django/settings.py
+++ schedulr_django/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y_ep7tx5fjrn3z1vyn)v043w-p0l$pp2kubcpsha3dn5d3%muv'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
