85030c486c517795c2bb21f924c41c2a0afc7746
kevin-m <kmclaughlinbtb@gmail.com>
2021-09-07 22:33:20 -0500

    updated shift model to use custom json views from serializers

--- schedulr/urls.py
+++ schedulr/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('companies/', views.CompanyList.as_view(), name='companies_list'),
-    path('companies/<int:pk>', views.CompanyDetail.as_view(), name='companies_detail')
+    path('companies/<int:pk>', views.CompanyDetail.as_view(), name='companies_detail'),
+    path('shifts/', views.shift_list, name='shifts_list'),
+    path('shifts/<int:pk>', views.shift_detail, name='shift_detail')
 ]
--- schedulr/views.py
+++ schedulr/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Company
-from .serializers import CompanySerializer
+from .models import Company, Shift
+from .serializers import CompanySerializer, ShiftSerializer
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -14,3 +15,22 @@ class CompanyList(generics.ListCreateAPIView):
 class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+# class ShiftList(generics.ListCreateAPIView):
+#     queryset = Shift.objects.all()
+#     serializer_class = ShiftSerializer
+
+# class ShiftDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Shift.objects.all()
+#     serializer_class = ShiftSerializer
+
+def shift_list(request):
+    shifts = Shift.objects.all().values('id', 'company', 'title', 'position', 'street', 'city', 'state', 'zip', 'lat', 'lng', 'uniform', 'description', 'on_site_contact', 'meeting_location', 'staff_needed', 'staff_claimed', 'payrate', 'billrate', 'start_time', 'end_time', 'created_at', 'created_by', 'company__name')
+    companies = Company.objects.all().values('id','name','contact_name', 'contact_email', 'contact_phone', 'contact_position' )
+    shifts_list = list(shifts)
+    return JsonResponse(shifts_list, safe=False)
+
+def shift_detail(request, pk):
+    shifts = Shift.objects.all().values('id', 'company', 'title', 'position', 'street', 'city', 'state', 'zip', 'lat', 'lng', 'uniform', 'description', 'on_site_contact', 'meeting_location', 'staff_needed', 'staff_claimed', 'payrate', 'billrate', 'start_time', 'end_time', 'created_at', 'created_by', 'company__name')
+    shift = shifts.filter(id=pk)
+    return JsonResponse(list(shift), safe=False)
